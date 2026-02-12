94d70485a921e9f34db8f31183c3045296ad620c
ADAM <ADAM@DESKTOP-4GUQRP5>
2026-02-12 16:32:05 +0100

    Phase 1 : création du projet. Phase 2 : premier test "Hello World !" Phase 3 : utilisation de Twig. Phase 4 : plusieurs pages.

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -1,21 +1,15 @@
 <?php
 
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-/**
- * Description of AccueilController
- *
- * @author ADAM
- */
-class AccueilController extends AbstractController {
+
+class VoyagesController extends AbstractController {
     
-    #[Route('/', name: 'acceuil')]
+    #[Route('/voyages', name: 'voyages')]
     public function index(): Response {
-       return $this->render("pages/accueil.html.twig");  
+       return $this->render("pages/voyages.html.twig");  
     }
-    
 }
